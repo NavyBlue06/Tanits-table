b6a6a52c5a687c992fddb31833d3d63b3a2cdb1d
NavyBlue06 <navaheierdal92@outlook.com>
2025-05-21 16:16:31 +0100

    Update ALLOWED_HOSTS for Heroku deployment

--- tanits_table/settings.py
+++ tanits_table/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Security
 SECRET_KEY = config("SECRET_KEY")
 DEBUG = config("DEBUG", default=False, cast=bool)
-ALLOWED_HOSTS = ["tanits-table.herokuapp.com", "localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["tanits-table-5445a6d81e90.herokuapp.com", "localhost", "127.0.0.1"]
 
 # Installed apps
 INSTALLED_APPS = [
